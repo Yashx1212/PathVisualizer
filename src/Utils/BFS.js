edb91b0a2bd87d7e9e98868370769cba51f83c2f
Yash <yash8164@gmail.com>
2023-05-02 22:40:01 +0530

    Refactored the previous code

--- src/Utils/BFS.js
+++ src/Utils/BFS.js
@@ -36,7 +36,7 @@ const BFS = (nodes) => {
 		for(let i=0;i<4;i++){
 			let row = curNode[0] + dir[i];
 			let col = curNode[1] + dir[i+1];
-			if(row>=0 && col>=0 && row < 20 && col <60 && !options[row][col].visited){
+			if(row >= 0 && col >= 0 && row < 20 && col < 60 && !options[row][col].visited){
 				options[row][col].visited = true;
 				options[row][col].parent = curNode;
 				options[row][col].dist = options[curNode[0]][curNode[1]].dist + 1;
